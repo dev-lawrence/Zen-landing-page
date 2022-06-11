4d9acf8d125e869b36e6dceec09eb9f5026a0e15
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-06-12 00:28:37 +0100

    finished the entire build and made some few changes

--- index.js
+++ index.js
@@ -4,17 +4,109 @@ const nav = document.querySelector('.nav');
 const menu = nav.querySelector('.menu');
 const menuLinks = menu.querySelectorAll('a');
 const menuBar = document.querySelector('.toggle-menu');
-const slider = document.querySelector('.slider__container');
-const slides = Array.from(slider.children);
-const slideSize = slides[0].getBoundingClientRect().width;
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-let currentSlide = 0;
-let auto = false;
-let slideTime = 7000;
-let slideInterval;
 const upArrow = document.querySelector('.page-up a');
 const cards = document.querySelectorAll('.fade-in');
+const page = document.querySelector('#home');
+const about = document.querySelector('#about');
+const body = document.querySelector('body');
+
+// FOR HOME AND ABOUT SECTION
+const galleryFun = () => {
+  // GALLERY SECTION
+  const galleryImg = document.querySelectorAll('.img');
+  const modal = document.querySelector('.modal-container');
+  const closeIcon = document.querySelector('.close');
+  const zoom = document.querySelector('.zoom');
+
+  galleryImg.forEach((img) => {
+    img.addEventListener('click', () => {
+      zoom.src = img.getAttribute('src');
+      modal.classList.add('show');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  closeIcon.addEventListener('click', () => {
+    modal.classList.remove('show');
+    document.body.style.overflow = 'auto';
+  });
+};
+
+// SLIDER FUNCTION FOR THE HOME SECTION
+if (body === page) {
+  const slider = document.querySelector('.slider__container');
+  const slides = Array.from(slider.children);
+  const slideSize = slides[0].getBoundingClientRect().width;
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  let currentSlide = 0;
+  let auto = true;
+  let slideTime = 7000;
+  let slideInterval;
+
+  const reset = () => {
+    slides.forEach((slide, index) => {
+      slide.style.left = `${slideSize * index}px`;
+    });
+
+    slides[0];
+  };
+
+  reset();
+
+  // move slide
+  const moveSlide = () => {
+    slider.style.transform = `translateX(-${currentSlide * slideSize}px)`;
+  };
+
+  // next slide
+  const nextSlide = () => {
+    currentSlide++;
+
+    if (currentSlide > slides.length - 1) {
+      currentSlide = 0;
+    }
+
+    if (auto) {
+      clearInterval(slideInterval);
+      slideInterval = setInterval(nextSlide, slideTime);
+    }
+
+    moveSlide();
+  };
+
+  // next slide
+  const prevSlide = () => {
+    currentSlide--;
+
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 1;
+    }
+
+    if (auto) {
+      clearInterval(slideInterval);
+      slideInterval = setInterval(nextSlide, slideTime);
+    }
+
+    moveSlide();
+  };
+
+  // auto slide
+  if (auto) {
+    slideInterval = setInterval(nextSlide, slideTime);
+  }
+
+  nextBtn.addEventListener('click', nextSlide);
+  prevBtn.addEventListener('click', prevSlide);
+
+  // GALLERY FUNCTION FOR THE HOME SECTION
+  galleryFun();
+}
+
+// GALLERY FUNCTION FOR THE ABOUT SECTION
+if (body === about) {
+  galleryFun();
+}
 
 // change header background color on scroll
 const showBg = () => {
@@ -48,60 +140,6 @@ menuLinks.forEach((link) => {
     link.classList.add('active');
   }
 });
-
-// SLIDERS
-// reset slide
-const reset = () => {
-  slides.forEach((slide, index) => {
-    slide.style.left = `${slideSize * index}px`;
-  });
-
-  slides[0];
-};
-
-reset();
-
-// move slide
-const moveSlide = () => {
-  slider.style.transform = `translateX(-${currentSlide * slideSize}px)`;
-};
-
-// next slide
-const nextSlide = () => {
-  currentSlide++;
-
-  if (currentSlide > slides.length - 1) {
-    currentSlide = 0;
-  }
-
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, slideTime);
-  }
-
-  moveSlide();
-};
-
-// next slide
-const prevSlide = () => {
-  currentSlide--;
-
-  if (currentSlide < 0) {
-    currentSlide = slides.length - 1;
-  }
-
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, slideTime);
-  }
-
-  moveSlide();
-};
-
-// auto slide
-if (auto) {
-  slideInterval = setInterval(nextSlide, slideTime);
-}
 
 // CARDS
 // fade in options
@@ -144,25 +182,3 @@ const pageUp = () => {
 // EVENT LISTENERS
 window.addEventListener('scroll', showBg);
 window.addEventListener('scroll', pageUp);
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
-
-// GALLERY SECTION
-const gallery = document.querySelector('.section-e .grid');
-const galleryImg = document.querySelectorAll('.img');
-const modal = document.querySelector('.modal-container');
-const closeIcon = document.querySelector('.close');
-const zoom = document.querySelector('.zoom');
-
-galleryImg.forEach((img) => {
-  img.addEventListener('click', () => {
-    zoom.src = img.getAttribute('src');
-    modal.classList.add('show');
-    document.body.style.overflow = 'hidden';
-  });
-});
-
-closeIcon.addEventListener('click', () => {
-  modal.classList.remove('show');
-  document.body.style.overflow = 'auto';
-});
